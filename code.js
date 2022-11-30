08f1c8b1e636fb8b08d95273e450f91212c0e7c3
Luke Calderon <luke.a.calderon@gmail.com>
2022-11-30 16:47:57 -0600

    fixed fork issue with project

--- code.js
+++ code.js
@@ -2,3 +2,12 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+//sayHello
+function sayHello(input){
+    // if(input === undefined || input === true || input === false)
+    if (typeof input !== "string"){
+        return "Hello, World!";
+    }
+    return "Hello, " + input + "!";
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,31 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+//sayHello
+describe( "sayHello", function(){
+    it('should be  defined function', function(){
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should be  defined string', function(){
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return "Hello, Jane!" when passed "Jane"', function(){
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!" when passed "Alex"', function(){
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return "Hello, Pat!" when passed "Pat"', function(){
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it('should return "Hello, World!" when called without input', function(){
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!" when passed boolean true', function(){
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!" when passed boolean false', function(){
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+});
